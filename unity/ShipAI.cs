3bcbce609f40a60672be1c68d3263cac34028f73
Drew Gonzales <6912711+drewgonzales360@users.noreply.github.com>
2020-10-09 09:25:56 -0700

    Update ShipAI.cs

--- unity/ShipAI.cs
+++ unity/ShipAI.cs
@@ -1,11 +1,11 @@
-using System;
+    using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace UnityStandardAssets.Vehicles.Aeroplane
 {
     [RequireComponent(typeof (AeroplaneController))]
-    public class ShipAI : MonoBehaviour
+    public class AeroplaneAiControl : MonoBehaviour
     {
         // This script represents an AI 'pilot' capable of flying the plane towards a designated target.
         // It sends the equivalent of the inputs that a user would send to the Aeroplane controller.
@@ -16,8 +16,9 @@ namespace UnityStandardAssets.Vehicles.Aeroplane
         [SerializeField] private float m_MaxClimbAngle = 45;            // The maximum angle that the AI will attempt to make plane can climb at
         [SerializeField] private float m_MaxRollAngle = 45;             // The maximum angle that the AI will attempt to u
         [SerializeField] private float m_SpeedEffect = 0.01f;           // This increases the effect of the controls based on the plane's speed.
+        [SerializeField] private float m_TakeoffHeight = 20;            // the AI will fly straight and only pitch upwards until reaching this height
         [SerializeField] private Transform m_Target;                    // the target to fly towards
-        public float ThrottleInput = 0.1f;
+
         private AeroplaneController m_AeroplaneController;  // The aeroplane controller that is used to move the plane
         private float m_RandomPerlin;                       // Used for generating random point on perlin noise so that the plane will wander off path slightly
         private bool m_TakenOff;                            // Has the plane taken off yet
@@ -28,6 +29,9 @@ namespace UnityStandardAssets.Vehicles.Aeroplane
         {
             // get the reference to the aeroplane controller, so we can send move input to it and read its current state.
             m_AeroplaneController = GetComponent<AeroplaneController>();
+
+            // pick a random perlin starting point for lateral wandering
+            m_RandomPerlin = Random.Range(0f, 100f);
         }
 
 
@@ -46,7 +50,7 @@ namespace UnityStandardAssets.Vehicles.Aeroplane
                 // make the plane wander from the path, useful for making the AI seem more human, less robotic.
                 Vector3 targetPos = m_Target.position +
                                     transform.right*
-                                    (Mathf.PerlinNoise(m_LateralWanderSpeed, m_RandomPerlin)*2 - 1)*
+                                    (Mathf.PerlinNoise(Time.time*m_LateralWanderSpeed, m_RandomPerlin)*2 - 1)*
                                     m_LateralWanderDistance;
 
                 // adjust the yaw and pitch towards the target
@@ -62,20 +66,44 @@ namespace UnityStandardAssets.Vehicles.Aeroplane
                 // calculate the difference between current pitch and desired pitch
                 float changePitch = targetAnglePitch - m_AeroplaneController.PitchAngle;
 
+                // AI always applies gentle forward throttle
+                const float throttleInput = 0.5f;
+
                 // AI applies elevator control (pitch, rotation around x) to reach the target angle
                 float pitchInput = changePitch*m_PitchSensitivity;
 
                 // clamp the planes roll
                 float desiredRoll = Mathf.Clamp(targetAngleYaw, -m_MaxRollAngle*Mathf.Deg2Rad, m_MaxRollAngle*Mathf.Deg2Rad);
-                var rollInput = -(m_AeroplaneController.RollAngle - desiredRoll)*m_RollSensitivity;
+                float yawInput = 0;
+                float rollInput = 0;
+                if (!m_TakenOff)
+                {
+                    // If the planes altitude is above m_TakeoffHeight we class this as taken off
+                    if (m_AeroplaneController.Altitude > m_TakeoffHeight)
+                    {
+                        m_TakenOff = true;
+                    }
+                }
+                else
+                {
+                    // now we have taken off to a safe height, we can use the rudder and ailerons to yaw and roll
+                    yawInput = targetAngleYaw;
+                    rollInput = -(m_AeroplaneController.RollAngle - desiredRoll)*m_RollSensitivity;
+                }
+
+                // adjust how fast the AI is changing the controls based on the speed. Faster speed = faster on the controls.
+                float currentSpeedEffect = 1 + (m_AeroplaneController.ForwardSpeed*m_SpeedEffect);
+                rollInput *= currentSpeedEffect;
+                pitchInput *= currentSpeedEffect;
+                yawInput *= currentSpeedEffect;
 
                 // pass the current input to the plane (false = because AI never uses air brakes!)
-                m_AeroplaneController.Move(rollInput, pitchInput, ThrottleInput, false);
+                m_AeroplaneController.Move(rollInput, pitchInput, yawInput, throttleInput, false);
             }
             else
             {
                 // no target set, send zeroed input to the planeW
-                m_AeroplaneController.Move(0, 0, 0, false);
+                m_AeroplaneController.Move(0, 0, 0, 0, false);
             }
         }
 
